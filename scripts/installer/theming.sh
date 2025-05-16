0d078d3b7992370528b19ea64c8767ef826ce1f1
Victor <imvictor.dev@outlook.com>
2025-05-15 23:22:32 -0300

    fix

--- scripts/installer/theming.sh
+++ scripts/installer/theming.sh
@@ -25,7 +25,7 @@ run_command "yay -S --sudoloop --noconfirm kvantum-theme-catppuccin-git" "Instal
 
 run_command "cp -r /home/$SUDO_USER/simple-hyprland/configs/kitty /home/$SUDO_USER/.config/" "Copy Catppuccin theme configuration for Kitty terminal" "yes" "no"
 
-run_command 'sudo rm -rf /usr/share/sddm/themes/* && sudo tar -xvf /home/$SUDO_USER/simple-hyprland/assets/sddm/sddm-catppuccin-mocha.tar.xz -C /usr/share/sddm/themes/' 'Install Catppuccin SDDM theme' 'yes' 'no'
+run_command "sudo rm -rf /usr/share/sddm/themes/* && sudo tar -xvf /home/$SUDO_USER/simple-hyprland/assets/sddm/sddm-catppuccin-mocha.tar.xz -C /usr/share/sddm/themes/" "Install Catppuccin SDDM theme" "yes" "no"
 
 # Add instructions to configure theming
 print_info "\nPost-installation instructions:"
